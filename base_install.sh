b834c2670512ba65d67b17623dcb1fd3a4b47d14
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2023-06-03 17:29:05 +0200

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -36,7 +36,7 @@ echo $hostname > /etc/hostname
 echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
 echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
-mkinitcpio -P
+#mkinitcpio -P
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
 read -p "Enter EFI partition: " efipartition
