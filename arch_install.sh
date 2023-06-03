a60f640e76b37a1316260d4ddb1060d4b82a2f5e
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2023-06-03 18:31:05 +0200

    Changed plasma to plasma-desktop

--- arch_install.sh
+++ arch_install.sh
@@ -18,5 +18,5 @@ fi
 read -p "Do you want to install KDE-Plasma? [y/n]: " answer
 if [[ $answer = y ]] ; then
   echo "Installing KDE-Plasma..."
-  sudo pacman -Syu xorg plasma plasma-wayland-session kde-applications --noconfirm && systemctl enable sddm
+  sudo pacman -Syu xorg plasma-desktop plasma-wayland-session kde-applications --noconfirm && systemctl enable sddm
 fi
