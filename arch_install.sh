1fcf4b5e697a7cf1652dc64bcc2297052e1e0ac8
SkyLightEffect <71511518+SkyLightEffect@users.noreply.github.com>
2023-06-03 17:49:45 +0200

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -10,7 +10,7 @@ sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 read -p "Do you need a base installation? [y/n]: " answer
 if [[ $answer = y ]] ; then
   chmod u+x $DOT/base_install.sh
-  .$DOT/base_install.sh
+  ./$DOT/base_install.sh
 fi
 
 read -p "Do you want to install KDE-Plasma? [y/n]: " answer
